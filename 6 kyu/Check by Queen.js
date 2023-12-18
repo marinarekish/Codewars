f4a1e3c1f11c217ac641ce42b874d5f825fa457b
Maryna Rekish <marinarekish@gmail.com>
2023-12-18 12:18:13 +0100

    update katas

---
+++ 6 kyu/Check by Queen.js
@@ -0,0 +1,55 @@
+/* Description:
+
+In a game of chess, a queen is the most powerful piece on the board. 
+She can move an unlimited number of squares in a straight line in any of 8 directions 
+(forwards, backwards, left, right, and each of the four diagonals in between).
+
+The diagram below shows the queen's influence from her current position - 
+she would be able to take any piece on a square marked with an 'X'.
+Image from: https://www.pinterest.com/pin/567453621770398092/ 
+
+An opponent's king who can be taken by the queen is said to be in 'check', 
+and would then need to find some way to escape this situation.
+
+In any normal game of chess, the queen would work with her army on an 8x8 board 
+to threaten the king in this way, and ultimately try to win the game. 
+However, for this kata, the queen will work by herself on a 5x5 board.
+
+The 5x5 chessboard will be represented as a 2 dimensional array, 
+(ie: an array containing 5 other arrays, each containing 
+5 single character elements). Empty spaces within each sub-array will be 
+represented by an asterix: "*", while one of these 25 elements will be r
+epresented by a "q" (queen) and a "k" (king). Both will be represented in lower case.
+
+The 2 dimensional chessboard array would look something like this:
+
+var board = [ [ '*', '*', '*', '*', '*' ],
+              [ '*', '*', '*', '*', 'k' ],
+              [ '*', '*', '*', '*', '*' ],
+              [ '*', 'q', '*', '*', '*' ],
+              [ '*', '*', '*', '*', '*' ] ];
+
+Your task is to write a function which will return true 
+if the king is in check, and false if he isn't.
+*/
+
+// solution
+function getCoordinates(char, array) {
+  for (let i = 0; i < array.length; i++) {
+    let j = array[i].indexOf(char);
+
+    if (j != -1) {
+      return [i, j];
+    }
+  }
+}
+
+function check(board) {
+  let [queenY, queenX] = getCoordinates("q", board);
+  let [kingY, kingX] = getCoordinates("k", board);
+
+  if (queenY == kingY) return true;
+  if (queenX == kingX) return true;
+  if (Math.abs(queenX - kingX) == Math.abs(queenY - kingY)) return true;
+  return false;
+}
